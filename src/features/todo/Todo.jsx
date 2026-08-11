57019b11490291618a161b138435f45e61c13d1f
Pavel Elyutin <elyutin.pavel@gmail.com>
2026-08-11 22:54:34 +0300

    add TodoTabs component

--- src/features/todo/Todo.jsx
+++ src/features/todo/Todo.jsx
@@ -1,9 +1,11 @@
 import TodoHeader from "./components/TodoHeader";
+import TodoTabs from "./components/TodoTabs";
 
 function ToDo() {
     return (
         <main>
             <TodoHeader title="Tasks" date="11 August 2026" />
+            <TodoTabs />
         </main>
     );
 }
