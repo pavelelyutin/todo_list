1e64233ddd32c40cf318487da9d42a1ab2c857c2
Pavel Elyutin <elyutin.pavel@gmail.com>
2026-08-13 07:00:39 +0300

    create Todo List component

---
+++ src/features/todo/data/tasks.js
@@ -0,0 +1,28 @@
+const tasks = [
+  {
+    id: 1,
+    title: 'buy milk',
+    status: 'todo',
+    createdAt: '2026-08-05T15:22:00',
+  },
+  {
+    id: 2,
+    title: 'buy bread',
+    status: 'done',
+    createdAt: '2026-08-05T15:22:00',
+  },
+  {
+    id: 3,
+    title: 'buy cucumber',
+    status: 'todo',
+    createdAt: '2026-08-05T15:22:00',
+  },
+  {
+    id: 4,
+    title: 'training',
+    status: 'in-progress',
+    createdAt: '2026-08-05T15:22:00',
+  },
+];
+
+export default tasks;
