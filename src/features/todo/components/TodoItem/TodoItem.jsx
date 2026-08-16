ffb3a3d475dfb03e929316be21a5b419b209fb8e
Pavel Elyutin <elyutin.pavel@gmail.com>
2026-08-16 22:10:11 +0300

    add task status styles

--- src/features/todo/components/TodoItem/TodoItem.jsx
+++ src/features/todo/components/TodoItem/TodoItem.jsx
@@ -2,13 +2,15 @@ import './TodoItem.css';
 import formatDate from '../../../../utils/formatDate';
 
 function TodoItem({ task, onToggle }) {
+  const isDone = task.status === 'done';
+
   return (
-    <li className="tasks__item task">
+    <li className={`tasks__item task ${isDone ? 'done' : ''}`}>
       <div className="task__main">
-        <button className={`task__checkbox ${task.status === 'done' ? 'done' : ''}`} onClick={() => onToggle(task.id)}></button>
+        <button className={`task__checkbox ${isDone ? 'done' : ''}`} onClick={() => onToggle(task.id)}></button>
 
         <div className="task__content">
-          <h3 className={`task__title ${task.status === 'done' ? 'done' : ''}`}>{task.title}</h3>
+          <h3 className={`task__title ${isDone ? 'done' : ''}`}>{task.title}</h3>
           <span className="task__date">{formatDate(task.createdAt)}</span>
         </div>
       </div>
